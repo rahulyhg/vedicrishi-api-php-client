59ea990e36feeae8fbb4b6e26ff4cbacfaeb3b7a
chandantiwari <chandanrishiraj@gmail.com>
2015-05-21 18:19:14 +0530

    Initial Skeleton Commit

---
+++ src/APIException.php
@@ -0,0 +1,38 @@
+<?php
+namespace AdvancedAstrologyAndHoroscopeLib;
+
+use Exception;
+
+class APIException extends Exception {
+    //private value store
+    private $responseCode;
+    
+    /*
+     * The HTTP response code from the API request
+     * @param string $reason the reason for raising an exception
+     * @param int $responseCode the HTTP response code from the API request
+     */
+    public function __construct($reason, $responseCode)
+    {
+        parent::__construct($reason, $responseCode, NULL);
+        $this->responseCode = $responseCode;
+    }
+
+    /*
+     * The HTTP response code from the API request
+     * @return int
+     */
+    public function getResponseCode()
+    {
+        return $this->responseCode;
+    }
+
+    /*
+     * The reason for raising an exception
+     * @return string
+     */
+    public function getReason()
+    {
+        return $this->message;
+    }
+}
